ffe072e6fa149a68d1248c246ee1f328351ccecc
Coding Mantras <rampalsinghpunia@gmail.com>
2024-08-17 15:48:57 +0530

    update config

--- config/urls.py
+++ config/urls.py
@@ -21,8 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls', namespace='users')),
-
+    path('users/', include('users.urls', namespace='users')),
+    path('', include('chat.urls', namespace='chat')),
 ]
 
 if settings.DEBUG:
