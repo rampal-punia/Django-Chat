db7769fd834fe89c7e04d16883c4b7211c625e9d
Coding Mantras <rampalsinghpunia@gmail.com>
2024-08-18 15:25:49 +0530

    update api

--- apps/chat/api.py
+++ apps/chat/api.py
@@ -1,10 +1,14 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
 
     def get_queryset(self):
         return Conversation.objects.filter(user=self.request.user)
@@ -12,6 +16,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
 
     def get_queryset(self):
         return Message.objects.filter(conversation__user=self.request.user)
